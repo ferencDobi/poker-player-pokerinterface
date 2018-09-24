f70fe813700896cea9adcaf52ac9f18424c4501f
mogyorosibalin <mogyorosibalin@gmail.com>
2018-09-24 11:25:32 +0200

    watching started hand

--- Player.js
+++ Player.js
@@ -6,10 +6,38 @@ class Player {
   static betRequest(gameState, bet) {
     let bid = Math.random() * (gameState.players[gameState.in_action].stack / 10);
 
+    let player = gameState.players[gameState.in_action];
+    let cards = gameState.community_cards;
+
+    if (this.smallCards(player.hole_cards) && this.notSameSuit(player.hole_cards) && this.cardsToFar(player.hole_cards)) {
+      bet(0);
+    }
+
     bet(Math.round(gameState.current_buy_in - gameState.players[gameState.in_action].bet + Math.max(bid, gameState.minimum_raise)));
   }
 
   static showdown(gameState) {
+  }
+
+  static getCardValue(card) {
+    const values = {"J": 11, "Q": 12, "K": 13, "A": 14};
+    if (isNaN(card.rank)) {
+      return values[card.rank]
+    } else {
+      return parseInt(card.rank);
+    }
+  }
+
+  static smallCards(cards) {
+    return (this.getCardValue(cards[0]) + this.getCardValue(cards[1])) / 2 < 7;
+  }
+
+  static notSameSuit(cards) {
+    return cards[0].suit !== cards[1].suit;
+  }
+
+  static cardsToFar(cards) {
+    return Math.abs(this.getCardValue(cards[0]) - this.getCardValue(cards[1])) > 3;
   }
 }
 
